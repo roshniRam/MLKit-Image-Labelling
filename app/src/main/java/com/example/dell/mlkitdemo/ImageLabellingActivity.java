6a1007a4c29c931d3de2ba9c07c7ccebc53392cb
roshniRam <roshni.ram001@gmail.com>
2018-12-07 13:22:19 +0530

    Add take photo from camera

--- app/src/main/java/com/example/dell/mlkitdemo/ImageLabellingActivity.java
+++ app/src/main/java/com/example/dell/mlkitdemo/ImageLabellingActivity.java
@@ -1,10 +1,21 @@
 package com.example.dell.mlkitdemo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -18,50 +29,133 @@ import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
 
 import java.util.List;
 
-public class ImageLabellingActivity extends AppCompatActivity {
+public class ImageLabellingActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView mImageView;
+    Button selectImageButton;
+    Button detectImageButton;
+    Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_labelling);
 
+        mImageView = findViewById(R.id.display_image);
+        selectImageButton = findViewById(R.id.select_image_button);
+        detectImageButton = findViewById(R.id.detect_image);
+        imageBitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.test6);
+
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage(view);
+            }
+        });
+
         FirebaseVisionLabelDetectorOptions options =
                 new FirebaseVisionLabelDetectorOptions.Builder()
                         .setConfidenceThreshold(0.8f)
                         .build();
 
-        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.test6);
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        detectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.test6);
+                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
 
-        FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
-                .getVisionLabelDetector();
+                FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
+                        .getVisionLabelDetector();
 
-        Task<List<FirebaseVisionLabel>> result =
-                detector.detectInImage(image)
-                        .addOnSuccessListener(
-                                new OnSuccessListener<List<FirebaseVisionLabel>>() {
-                                    @Override
-                                    public void onSuccess(List<FirebaseVisionLabel> labels) {
-                                        // Task completed successfully
-                                        // ...
-                                        for (FirebaseVisionLabel label: labels) {
-                                            String text = label.getLabel();
-                                            String entityId = label.getEntityId();
-                                            float confidence = label.getConfidence();
+                Task<List<FirebaseVisionLabel>> result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionLabel> labels) {
+                                                // Task completed successfully
+                                                // ...
+                                                for (FirebaseVisionLabel label: labels) {
+                                                    String text = label.getLabel();
+                                                    String entityId = label.getEntityId();
+                                                    float confidence = label.getConfidence();
 
-                                            Toast.makeText(ImageLabellingActivity.this, text, Toast.LENGTH_LONG).show();
-                                        }
-                                    }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Task failed with an exception
-                                        // ...
-                                    }
-                                });
+                                                    Toast.makeText(ImageLabellingActivity.this, text, Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                // ...
+                                            }
+                                        });
+
+            }
+        });
+
 
 
     }
+
+    public void selectImage(View v){
+        PopupMenu popupMenu = new PopupMenu(this,v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.options_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.take_photo:
+                if (ContextCompat.checkSelfPermission(ImageLabellingActivity.this, android.Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(ImageLabellingActivity.this,
+                            android.Manifest.permission.CAMERA)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                    } else {
+                        // No explanation needed; request the permission
+                        ActivityCompat.requestPermissions(ImageLabellingActivity.this,
+                                new String[]{Manifest.permission.CAMERA},1);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }else{
+                    dispatchTakePictureIntent();
+                }
+                return true;
+            case R.id.cancel:
+                finish();
+                System.exit(0);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
 }
